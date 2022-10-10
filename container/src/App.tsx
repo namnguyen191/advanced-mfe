263945ed6f6b30e327a0d244e7f4137635827de4
Nam Nguyen <hoangnamnguyen191@gmail.com>
2022-10-10 19:58:19 -0400

    feat: refractor container routing to have share layout

--- container/src/App.tsx
+++ container/src/App.tsx
@@ -1,34 +1,12 @@
-import { CacheProvider } from '@emotion/react';
 import React from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { Header } from './components/Header';
-// import { MarkettingApp } from './components/MarkettingApp';
-import createCache from '@emotion/cache';
-import { AuthApp } from './components/AuthApp';
-import { MarkettingApp } from './components/MarkettingApp';
-
-const router = createBrowserRouter([
-  {
-    path: '*',
-    element: (
-      <>
-        <Header />
-        <MarkettingApp />
-        <AuthApp />
-      </>
-    )
-  }
-]);
-
-export const muiCache = createCache({
-  key: 'mui',
-  prepend: true
-});
 
 export const App: React.FC = () => {
   return (
-    <CacheProvider value={muiCache}>
-      <RouterProvider router={router} />
-    </CacheProvider>
+    <>
+      <Header />
+      <Outlet />
+    </>
   );
 };
--- container/src/bootstrap.tsx
+++ container/src/bootstrap.tsx
@@ -1,12 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+import { CacheProvider } from '@emotion/react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import createCache from '@emotion/cache';
+import { AuthApp } from './components/AuthApp';
+import { MarkettingApp } from './components/MarkettingApp';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <MarkettingApp />
+      },
+      {
+        path: '/auth/*',
+        element: <AuthApp />
+      }
+    ]
+  }
+]);
+
+export const muiCache = createCache({
+  key: 'mui',
+  prepend: true
+});
 
 export const mount = (el: Element): void => {
   const root = ReactDOM.createRoot(el);
   root.render(
     <React.StrictMode>
-      <App />
+      <CacheProvider value={muiCache}>
+        <RouterProvider router={router} />
+      </CacheProvider>
     </React.StrictMode>
   );
 };
